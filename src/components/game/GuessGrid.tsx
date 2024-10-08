bf6c5d40dddef67a1c1044760922d2e128f40274
Beto Carlos <me@beto.codes>
2024-10-07 22:02:17 -0600

    Added color for status

--- src/components/game/GuessGrid.tsx
+++ src/components/game/GuessGrid.tsx
@@ -1,27 +1,31 @@
 import { useContext } from 'react'
 import { range } from 'lodash-es'
-import { NUM_OF_GUESSES_ALLOWED } from '../../constants'
+import { Word } from './Word'
 import { GuessContext } from '../../providers/GuessProvider'
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants'
 import { checkGuess } from '../../utils/game-helpers'
+import { type Result } from '../../schema'
 
 export const GuessGrid = () => {
 	const { guesses } = useContext(GuessContext)
-	const gridGuesses = guesses.map((guess) => guess.word.split(''))
+
+	const gridGuesses = range(NUM_OF_GUESSES_ALLOWED).map((row) => {
+		if (!guesses[row]) return range(5).map(() => ' ')
+		return checkGuess(guesses[row].word, 'LEARN') // -> Change second parameter
+	})
 
 	return (
 		<div className="space-y-2">
-			{range(NUM_OF_GUESSES_ALLOWED).map((row) => (
+			{gridGuesses.map((row, i) => (
 				<div
-					key={row}
+					key={i}
 					className="group grid grid-cols-5 gap-2"
 				>
-					{range(5).map((col) => (
-						<span
-							key={col}
-							className="flex aspect-square items-center justify-center rounded-sm border-2 border-slate-200 text-lg font-bold group-first:first:rounded-tl-xl group-first:last:rounded-tr-xl group-last:first:rounded-bl-xl group-last:last:rounded-br-xl"
-						>
-							{gridGuesses[row] ? gridGuesses[row][col] : ''}
-						</span>
+					{row?.map((result, i) => (
+						<Word
+							key={i}
+							result={result as string | Result}
+						/>
 					))}
 				</div>
 			))}
--- src/schema.ts
+++ src/schema.ts
@@ -2,3 +2,8 @@ export interface Guess {
 	id: string
 	word: string
 }
+
+export interface Result {
+	letter: string
+	status: 'incorrect' | 'correct' | 'misplaced'
+}
