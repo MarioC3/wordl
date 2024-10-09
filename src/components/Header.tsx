50cf20ff4b43f51bcf53a3785d9b7e7c0e0225ca
Beto Carlos <me@beto.codes>
2024-10-09 12:23:51 -0600

    Added some character

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export const Header = () => {
 	return (
 		<header className="text-center">
-			<h1 className="font-serif text-5xl font-bold tracking-normal">Wordl</h1>
+			<h1 className="font-serif text-5xl font-bold tracking-normal">Wordl*</h1>
 		</header>
 	)
 }
