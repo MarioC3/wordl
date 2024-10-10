71c4908bdba2e170df16b0cdf1bee1438b557950
Beto Carlos <me@beto.codes>
2024-10-09 18:02:55 -0600

    link to site

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,11 +3,18 @@ import sigUrl from '../assets/sig.svg'
 export const Footer = () => {
 	return (
 		<div className="flex flex-col items-center gap-2">
-			<img
-				className="w-12"
-				src={sigUrl}
-				alt="Beto's logo"
-			/>
+			{/* eslint-disable-next-line react/jsx-no-target-blank */}
+			<a
+				href="https://beto.codes/"
+				target="_blank"
+			>
+				<img
+					className="w-12"
+					src={sigUrl}
+					alt="Beto's logo"
+				/>
+			</a>
+
 			<span className="text-xs">© 2024 beto carlos</span>
 		</div>
 	)
