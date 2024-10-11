70bf4b3fbdc3c33d6a592ad3884492a4af543a73
Beto Carlos <me@beto.codes>
2024-10-11 11:46:39 -0600

    slight changes to animations

--- src/components/game/Game.tsx
+++ src/components/game/Game.tsx
@@ -1,16 +1,15 @@
-import { type PropsWithChildren, useContext } from 'react'
+import { useContext } from 'react'
 import { m, AnimatePresence } from 'framer-motion'
-import { twMerge } from 'tailwind-merge'
 import { GuessProvider } from '../../providers/GuessProvider'
 import { GameContext } from '../../providers/GameProvider'
-import { WordsContext } from '../../providers/WordsProvider'
 import { GuessInput } from './GuessInput'
 import { GuessGrid } from './GuessGrid'
+import { EndGameBanner } from './EndGameBanner'
 
 const variants = {
-	initial: { opacity: 0, y: -15 },
-	visible: { opacity: 1, y: 0 },
-	exit: { opacity: 0, y: 15 },
+	initial: { opacity: 0, x: -15 },
+	visible: { opacity: 1, x: 0 },
+	exit: { opacity: 0, x: 15 },
 }
 
 export const Game = () => {
@@ -31,7 +30,7 @@ export const Game = () => {
 								initial="initial"
 								animate="visible"
 								exit="exit"
-								transition={{ type: 'spring', duration: 0.4, bounce: 0 }}
+								transition={{ type: 'spring', duration: 0.2, bounce: 0 }}
 							>
 								<GuessInput />
 							</m.div>
@@ -42,7 +41,7 @@ export const Game = () => {
 								initial="initial"
 								animate="visible"
 								exit="exit"
-								transition={{ type: 'spring', duration: 0.4, bounce: 0 }}
+								transition={{ type: 'spring', duration: 0.2, bounce: 0 }}
 							>
 								<EndGameBanner
 									status={gameStatus}
@@ -54,33 +53,5 @@ export const Game = () => {
 				</div>
 			</div>
 		</GuessProvider>
-	)
-}
-
-interface EndGameBannerProps {
-	status: 'win' | 'lose'
-	tries: number
-}
-export const EndGameBanner = ({ status, tries }: PropsWithChildren<EndGameBannerProps>) => {
-	const { answer } = useContext(WordsContext)
-
-	const text = {
-		win: {
-			title: 'You win!',
-			text: `Got it in ${tries.toString()} guesses.`,
-		},
-		lose: {
-			title: 'Sorry!',
-			text: `The correct answer is ${answer}.`,
-		},
-	}
-
-	const statusClass = status === 'win' ? 'bg-teal-800' : 'bg-rose-600'
-
-	return (
-		<div className={twMerge('rounded-md p-4 text-center text-white', statusClass)}>
-			<span className="text-xl font-bold">{text[status].title}</span>
-			<p>{text[status].text}</p>
-		</div>
 	)
 }
